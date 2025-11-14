c02c501019044fef3e8a6b912c4f32d528fb74e4
kimtaehyeong99 <kth@robotis.com>
2025-11-14 16:51:51 +0900

    Refactor FFW_BG2_CFG_WITHOUT_MIMIC to use deepcopy for configuration cloning

--- source/robotis_lab/robotis_lab/assets/robots/FFW_BG2.py
+++ source/robotis_lab/robotis_lab/assets/robots/FFW_BG2.py
@@ -14,6 +14,8 @@
 #
 # Author: Taehyeong Kim
 
+from copy import deepcopy
+
 from isaaclab.sim import UsdFileCfg, RigidBodyPropertiesCfg, ArticulationRootPropertiesCfg
 from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.actuators import ImplicitActuatorCfg
@@ -97,79 +99,5 @@ FFW_BG2_CFG = ArticulationCfg(
     }
 )
 
-FFW_BG2_CFG_WITHOUT_MIMIC = ArticulationCfg(
-    spawn=UsdFileCfg(
-        usd_path=f"{ROBOTIS_LAB_ASSETS_DATA_DIR}/robots/FFW/FFW_BG2_without_mimic.usd",
-        rigid_props=RigidBodyPropertiesCfg(
-            disable_gravity=True,
-            max_depenetration_velocity=5.0,
-        ),
-        articulation_props=ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True,
-            solver_position_iteration_count=8,
-            solver_velocity_iteration_count=2,
-        ),
-        activate_contact_sensors=True,
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        joint_pos={
-            # Lift joint
-            "lift_joint": 0.0,
-
-            # Left arm joints
-            **{f"arm_l_joint{i + 1}": 0.0 for i in range(7)},
-            # Right arm joints
-            **{f"arm_r_joint{i + 1}": 0.0 for i in range(7)},
-
-            # Left and right gripper joints
-            **{f"gripper_l_joint{i + 1}": 0.0 for i in range(4)},
-            **{f"gripper_r_joint{i + 1}": 0.0 for i in range(4)},
-
-            # Head joints
-            **{f"head_joint{i + 1}": 0.0 for i in range(2)},
-        },
-    ),
-    actuators={
-        # Actuator for vertical lift joint
-        "lift": ImplicitActuatorCfg(
-            joint_names_expr=["lift_joint"],
-            velocity_limit_sim=1.0,
-            effort_limit_sim=1000.0,
-            stiffness=10000.0,
-            damping=1000.0,
-        ),
-
-        # Actuators for both arms
-        "arms": ImplicitActuatorCfg(
-            joint_names_expr=[
-                "arm_l_joint[1-7]",
-                "arm_r_joint[1-7]",
-            ],
-            velocity_limit_sim=10.0,
-            effort_limit_sim=1000.0,
-            stiffness=400.0,
-            damping=80.0,
-        ),
-
-        # Actuators for grippers
-        "grippers": ImplicitActuatorCfg(
-            joint_names_expr=[
-                "gripper_l_joint[1-4]",
-                "gripper_r_joint[1-4]",
-            ],
-            velocity_limit_sim=6.0,
-            effort_limit_sim=30.0,
-            stiffness=20.0,
-            damping=5.0,
-        ),
-
-        # Actuators for head joints
-        "head": ImplicitActuatorCfg(
-            joint_names_expr=["head_joint1", "head_joint2"],
-            velocity_limit_sim=5.0,
-            effort_limit_sim=300.0,
-            stiffness=200.0,
-            damping=50.0,
-        ),
-    }
-)
+FFW_BG2_CFG_WITHOUT_MIMIC = deepcopy(FFW_BG2_CFG)
+FFW_BG2_CFG_WITHOUT_MIMIC.spawn.usd_path = f'{ROBOTIS_LAB_ASSETS_DATA_DIR}/robots/FFW/FFW_BG2_without_mimic.usd'
